c5930c8d6f22ba7688c3d3b1d0a547a5f181cd5a
DanielFalcon <danielfalcon.87@gmail.com>
2026-04-15 15:58:00 -0600

    Curso item added

--- src/MasterNet.Persistence/MainEntry.cs
+++ src/MasterNet.Persistence/MainEntry.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MasterNet.Persistence;
+using MasterNet.Domain;
 
 var service = new ServiceCollection();
 service.AddLogging(l =>
@@ -16,6 +17,24 @@ try
     var context = scope.ServiceProvider.GetRequiredService<MasterNetDbContext>();
     await context.Database.MigrateAsync();
     Console.WriteLine("Migración y seeding completados exitosamente.");
+
+    var newCurso = new Curso
+    {
+        Id = Guid.NewGuid(),
+        Titulo = "Programacion con .Net",
+        Descripcion = "Aprende .Net",
+        FechaPublicacion = DateTime.Now
+    };
+    
+    context.Cursos!.Add(newCurso);
+    await context.SaveChangesAsync();
+
+    var cursos = await context.Cursos!.ToListAsync();
+
+    foreach (var item in cursos)
+    {
+        Console.WriteLine($"{item.Id} - {item.Titulo}");   
+    }
 }
 catch (Exception ex)
 {
